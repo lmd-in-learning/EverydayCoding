401b65f4c589f0cc9c0bf96210c30261124f88d5
lmd-in-learning <492556709@qq.com>
2016-12-08 22:30:54 +0800

    complement the Topological class

--- DailyCoding/Topological.cpp
+++ DailyCoding/Topological.cpp
@@ -7,3 +7,32 @@
 //
 
 #include "Topological.h"
+#include "DirectionGraph.h"
+#include "DepthFirstOrder.h"
+#include "DirectedCycle.h"
+
+Topological::Topological(DirectionGraph& graph)
+{
+    DirectedCycle* pCyclefinder = new DirectedCycle(graph);
+    if (!pCyclefinder->HasCycle())
+    {
+        DepthFirstOrder* pDfs = new DepthFirstOrder(graph);
+        
+        m_queuePre         = pDfs->Pre();
+        m_queuePost        = pDfs->Post();
+        m_stackReversePost = pDfs->ReversePost();
+    }
+}
+
+Topological::~Topological()
+{
+    
+}
+
+bool Topological::IsDAG() const
+{
+    //数据不为空
+    return (!m_queuePre.empty() &&
+            !m_queuePost.empty() &&
+            !m_stackReversePost.empty());
+}
--- DailyCoding/Topological.h
+++ DailyCoding/Topological.h
@@ -10,5 +10,46 @@
 #define Topological_h
 
 #include <stdio.h>
+#include <stack>
+#include <queue>
+#include <vector>
+
+using std::stack;
+using std::queue;
+using std::vector;
+
+class DirectionGraph;
+
+class Topological
+{
+    //constructor
+public:
+    Topological(DirectionGraph& graph);
+    ~Topological();
+    
+    //public functions
+public:
+    //return true if group is directed graph without loop
+    bool IsDAG() const;
+    
+    //java 有Iterable<Integer>，此处不太适用，故分别写出来
+    //拓扑有序的前序顶点，在递归调用之前将顶点加入队列
+    queue<int> Pre();
+    
+    //拓扑有序的后序顶点，在递归调用之后将顶点加入队列
+    queue<int> Post();
+    
+    //拓扑有序的逆后序顶点，在递归调用之后将顶点压入栈
+    stack<int> ReversePost();
+    
+    //private members
+private:
+    queue<int> m_queuePre;
+    
+    queue<int> m_queuePost;
+    
+    stack<int> m_stackReversePost;
+    
+};
 
 #endif /* Topological_h */
