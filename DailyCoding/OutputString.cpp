265c264ae9f13da9d99a66c9f6ba39aed3cff45c
lmd-in-learning <492556709@qq.com>
2016-12-11 16:03:49 +0800

    add outputstring functions

---
+++ DailyCoding/OutputString.cpp
@@ -0,0 +1,18 @@
+//
+//  OutputString.cpp
+//  DailyCoding
+//
+//  Created by limingding on 12/11/16.
+//  Copyright © 2016 limingding. All rights reserved.
+//
+
+#include "OutputString.h"
+
+
+void ArrayIntToString(ArrayInt& array)
+{
+    for (int i = 0; i < array.GetSize(); ++i)
+    {
+//        fprintf(stdout, "a[%d] = %d\n", i, array[i]);
+    }
+}
---
+++ DailyCoding/OutputString.h
@@ -0,0 +1,17 @@
+//
+//  OutputString.hpp
+//  DailyCoding
+//
+//  Created by limingding on 12/11/16.
+//  Copyright © 2016 limingding. All rights reserved.
+//
+
+#ifndef OutputString_h
+#define OutputString_h
+
+#include <stdio.h>
+#include "ArrayInt.h"
+
+void ArrayIntToString(ArrayInt& array);
+
+#endif /* OutputString_h */
